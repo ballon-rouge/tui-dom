1914681bf05fddadfc1f4b260298fa2b968f2895
Alexandre Massonneau <alex@redballoonsecurity.com>
2020-07-07 08:52:51 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+from setuptools import setup
+
+
+requirements = [
+    'colorama',
+    "asciimatics",
+]
+if sys.version_info < (3, 7):
+    requirements.append("dataclasses")
+
+setup(
+    name="rbs_tui_dom",
+    version="1.0.0",
+    description="The RBS Terminal User Interface(TUI) framework",
+    packages=[
+        "rbs_tui_dom",
+        "rbs_tui_dom.dom",
+        "rbs_tui_dom.extra",
+    ],
+    ext_modules=[],
+    install_requires=['LatLon'],
+    extras_require={
+        "frontend": [
+
+        ],
+    }
+)
