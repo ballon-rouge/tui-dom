4df752b42d727f00459f7f725de788e63a76e592
Andrew O'Brien <aob@redballoonsecurity.com>
2020-07-07 13:49:44 -0400

    Add long description to setup.py, bump to 1.0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from setuptools import setup
+from os import path
 
 
 requirements = [
@@ -11,10 +12,16 @@ requirements = [
 if sys.version_info < (3, 7):
     requirements.append("dataclasses")
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="rbs_tui_dom",
-    version="1.0.0",
+    version="1.0.1",
     description="The RBS Terminal User Interface(TUI) framework",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[
         "rbs_tui_dom",
         "rbs_tui_dom.dom",
